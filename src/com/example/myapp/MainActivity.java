a49b1795ec13527914a4f9ab0b049fd7f0d66607
Martin Cocaro <martin.cocaro@gmail.com>
2015-04-06 16:27:04 -0300

    Initial version

---
+++ src/com/example/myapp/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.myapp;
+
+import com.thrivecom.ringcaptcha.RingcaptchaApplication;
+import com.thrivecom.ringcaptcha.RingcaptchaApplicationHandler;
+import com.thrivecom.ringcaptcha.RingcaptchaVerification;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends ActionBarActivity {
+
+	private static final String APP_KEY = "4ozucaco7e2o1iqy3ubi";
+    private static final String API_SECRET = "vivy4yzyre2e1edy6eru";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		RingcaptchaApplication.onboard(getApplicationContext(), APP_KEY, API_SECRET,new RingcaptchaApplicationHandler() {
+			
+			@Override
+			public void onSuccess(RingcaptchaVerification arg0) {
+				// TODO Auto-generated method stub
+			}
+			
+			@Override
+			public void onCancel() {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
